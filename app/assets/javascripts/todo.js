c29862a7b54b0136097a207d179f0f8760061afa
Joe Shepherd <joeshepmedia@gmail.com>
2013-06-10 08:22:03 -0500

    Merge stashed stuff

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
   $('#done').click(function() {
-    $('.tasks').css("color","red");
+    if ($('#done').is(':checked'))
   $.ajax({
-    // success: complete_task_path(@list.id,task.id)
     url: $(this).data('href'),
     type: 'PUT',
     dataType: 'html',
-    success: function(data, textStatus, jqXHR) {
-      // Do something here like refresh show page
-    }
+    success: alert("Yay!")
   });
  }); //end click
 }); //end ready

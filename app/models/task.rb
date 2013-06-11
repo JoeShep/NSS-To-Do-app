d72db15b770fe3247634b188d66f62a7892b6d3c
Joe Shepherd <joeshepmedia@gmail.com>
2013-06-10 19:47:45 -0500

    Update task.rb
    Change association to use join table

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ActiveRecord::Base
   attr_accessible :title
-  belongs_to :list
+  has_and_belongs_to_many :lists
   scope :done, where(:done => true)
   scope :not_done, where(:done => false)
 end
